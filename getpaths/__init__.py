488a1318fa866c9fdc81bc9521a26378d0901f89
Kyle <s.t.o.r.m.detective@gmail.com>
2021-09-01 04:33:39 -0500

    got rid of accidental doubling, reformatted with black

--- getpaths/__init__.py
+++ getpaths/__init__.py
@@ -1,8 +1,8 @@
-'''
+"""
 Made by Jv Kyle Eclarin
 
 Borrows heavily from pathlib, os, shutil, and glob
-'''
+"""
 
 import os as _os
 import sys as _sys
@@ -14,12 +14,11 @@ from copy import deepcopy as _deepcopy
 from importlib.machinery import SourceFileLoader as _SourceFileLoader
 
 
-
 # find path of the file doing the importing
 filedir = None
 
 for frame in _inspect.stack()[1:]:
-    if frame.filename[0] != '<':
+    if frame.filename[0] != "<":
         filedir = frame.filename
         break
 if filedir == None:
@@ -29,54 +28,55 @@ if filedir == None:
 
 
 class getpath(str):
-    '''
-# IMPORTING
+    """
+    # IMPORTING
 
-from getpaths import getpath
-
-
-# USING CURRENT FILE PATH
-
-current_dir = getpath() # current file's path
+    from getpaths import getpath
 
 
-# USER-SPECIFIED PATH
+    # USING CURRENT FILE PATH
 
-custom_dir = getpath('a','b',custom=True)
-print(custom_dir)
-# /a/b
+    current_dir = getpath() # current file's path
 
 
-# ADD FILES
+    # USER-SPECIFIED PATH
 
-custom_dir/'example.txt'
-# /a/b/example.txt
-
-custom_dir.add('/a/example.txt')
-# /a/b/example.txt
+    custom_dir = getpath('a','b',custom=True)
+    print(custom_dir)
+    # /a/b
 
 
-#  GO UP THE DIRECTORY
+    # ADD FILES
 
-custom_dir = getpath('a','b',custom=True)
+    custom_dir/'example.txt'
+    # /a/b/example.txt
 
-custom_dir/'..'
-# /a
-
-custom_dir.add('..')
-# /a
-
-custom_dir.up(1)
-# /a
+    custom_dir.add('/a/example.txt')
+    # /a/b/example.txt
 
 
-# LIST EXISTING FILES AND FOLDERS
+    #  GO UP THE DIRECTORY
 
-custom_dir.ls()
-# /a/b/example.txt
-# throws error if path does not exist
+    custom_dir = getpath('a','b',custom=True)
 
-    '''
+    custom_dir/'..'
+    # /a
+
+    custom_dir.add('..')
+    # /a
+
+    custom_dir.up(1)
+    # /a
+
+
+    # LIST EXISTING FILES AND FOLDERS
+
+    custom_dir.ls()
+    # /a/b/example.txt
+    # throws error if path does not exist
+
+    """
+
     def __new__(cls, *args, custom=False, start_at_root=True):
 
         if custom:
@@ -85,185 +85,185 @@ custom_dir.ls()
             paths = [_os.path.dirname(filedir)]
 
         for arguments in args:
-            if arguments == '..':
+            if arguments == "..":
                 paths[0] = _os.path.split(paths[0])[0]
             else:
                 paths.append(arguments)
-        
-        
+
         path = _os.sep.join(paths)
 
         # get rid of accidental doubling
-        if custom:
-            double = _os.path.sep + _os.path.sep
-            while double in path:
-                path = path.replace(double, _os.path.sep)
-                
+        double = _os.path.sep + _os.path.sep
+        while double in path:
+            path = path.replace(double, _os.path.sep)
+
         if start_at_root == True:
             if path[0] != _os.path.sep:
                 path = _os.path.sep + path
 
         return str.__new__(cls, path)
-    
+
     def __init(self):
         super().__init__(self)
-    
+
     def add(self, *args):
         return self.__truediv__(*args)
-    
+
     def __truediv__(self, *args):
         # same as add
         current_path = _deepcopy(self.__str__())
 
         paths = [current_path]
-        
+
         for arguments in args:
-            # convert arguments to 
+            # convert arguments to
             if type(arguments) != type(str()):
                 try:
                     arguments = str(arguments)
                 except:
-                    print('path could not be converting to a string')
+                    print("path could not be converting to a string")
                     raise TypeError
 
-            if arguments == '..':
+            if arguments == "..":
                 paths[0] = _os.path.split(paths[0])[0]
             else:
                 paths.append(arguments)
-        
+
         path = _os.sep.join(paths)
         return getpath(path, custom=True)
-    
+
     def ls(self, *args, full=False):
-        '''
+        """
         same as os.listdir()
 
         if full=True, then this returns a list of absolute paths
-        '''
+        """
         paths = args
-        
+
         path = _os.sep.join([self.__str__(), _os.sep.join(paths)])
 
         files_and_stuff = _os.listdir(path)
-        
+
         if full == True:
             return [getpath(path, custom=True) for path in files_and_stuff]
         else:
             return files_and_stuff
-    
+
     def find(self, path, *args, **kwargs):
-        '''
+        """
         same as glob.glob()
-        '''
+        """
         files_and_stuff = _glob(_os.path.join(self.__str__(), path), *args, **kwargs)
         return [getpath(path, custom=True) for path in files_and_stuff]
-    
+
     def isfile(self):
         return _os.path.isfile(self.__str__())
-    
+
     def isdir(self):
         return _os.path.isdir(self.__str__())
-    
+
     def exists(self):
-        '''
-        returns 
+        """
+        returns
         same as os.path.exists()
-        '''
+        """
         return _os.path.exists(self.__str__())
-    
+
     def copyfile(self, destination, **kwargs):
-        '''
+        """
         copies current path if current path is a file to destination
         same as shutil.copyfile
-        '''
+        """
         assert _os.path.isfile(self.__str__()) == True
         _shutil.copyfile(self.__str__(), destination, **kwargs)
-    
+
     def copydir(self, destination, **kwargs):
-        '''
+        """
         copies current path if current path is a file to destination
         same as shutil.copyfile
-        '''
+        """
         assert _os.path.isdir(self.__str__()) == True
         _shutil.copy(self.__str__(), destination, **kwargs)
-    
+
     def move(self, destination, **kwargs):
-        '''
+        """
         moves current path to destination
-        '''
+        """
         assert _os.path.exists(self.__str__()) == True
-        _shutil.move(self.__str__(), destination, **kwargs) 
-    
+        _shutil.move(self.__str__(), destination, **kwargs)
+
     def mkdir(self, **kwargs):
-        '''
+        """
         same as os.makedirs()
-        '''
+        """
         assert _os.path.isdir(self.__str__()) == True
         _os.makedirs(self.__str__(), **kwargs)
-    
+
     def rmdir(self, **kwargs):
-        '''
+        """
         same as os.rmdir()
-        '''
+        """
         assert _os.path.isdir(self.__str__()) == True
         _os.rmdir(self.__str__(), **kwargs)
-    
-    def mkfile(self,  data='', *args, **kwargs):
+
+    def mkfile(self, data="", *args, **kwargs):
         with open(self.__str__(), *args, **kwargs) as file_handler:
             file_handler.write(data)
-    
+
     def rmfile(self, **kwargs):
         assert _os.path.isdir(self.__str__()) == True
         _os.remove(self.__str__(), **kwargs)
-    
+
     def write(self, *args, **kwargs):
         self.mkfile(*args, **kwargs)
-    
+
     def read(self, *args, **kwargs):
         assert _os.path.isfile(self.__str__()) == True
         with open(self.__str__(), *args, **kwargs) as file_handler:
             file_handler.read(data)
-    
-    def readfast(self, name='', *args, **kwargs):
-        '''
+
+    def readfast(self, name="", *args, **kwargs):
+        """
         generator for reading large files quickly
         does not open entire file, but yields it one line at a time
-        '''
-        
+        """
+
         assert _os.path.isfile(self.__str__()) == True
 
         with open(self.__str__(), *args, **kwargs) as file_handler:
             for line in file_handler:
                 yield line
-        
+
     def up(self, num, *args):
         paths = []
         for arguments in args:
             paths.append(arguments)
-        
+
         path = _os.sep.join([self.__str__(), _os.sep.join(paths)])
 
-        times_up = ['..' for i in range(num+1)]
+        times_up = [".." for i in range(num + 1)]
         return getpath(path, *times_up, custom=True)
-    
+
     def splitpath(self, full=False):
-        '''
+        """
         same as os.path.split
         if full=True, returns list of strings
-        '''
+        """
         path = _os.path.split(self.__str__())
         if full == True:
             return self.__str__().split(os.path.sep)
         else:
             return [getpath(path[0], custom=True), path[1]]
-    
+
+    def string(self):
+        return str(_Path(self.__str__()))
 
 
 def importfile(path):
-    '''
+    """
     same as importlib.util.SourceFileLoader()
     returns file functions, classes, and global variables from a python file
-    '''
+    """
     path = getpath(path, custom=True)
     assert _os.path.isfile(path) == True
 
@@ -271,16 +271,16 @@ def importfile(path):
     return file_handler
 
 
-
 class importdir:
-    '''
+    """
     imports .py files from directory
-    '''
+    """
+
     def __init__(self, path):
         assert _os.path.isdir(path) == True
         path = getpath(path, custom=True)
 
-        python_files = path.find('*.py')
+        python_files = path.find("*.py")
         if len(python_files) == 0:
             return
 
@@ -289,8 +289,7 @@ class importdir:
             try:
                 setattr(self, name, importfile(python_file))
             except:
-                print('was not able to to load modules from', python_file)
-
+                print("was not able to to load modules from", python_file)
 
 
 cwd = getpath(str(_Path.cwd()), custom=True)
